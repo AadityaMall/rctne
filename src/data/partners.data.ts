965894d932d06bb751e76f876d3dbac7ede83f72
AadityaMall <aadityarmall@gmail.com>
2026-09-16 09:50:54 +0530

    chore: change partners data

--- src/data/partners.data.ts
+++ src/data/partners.data.ts
@@ -2,9 +2,6 @@ import type { Partner } from "@/types/content.types"
 
 export const partners: Partner[] = [
   { name: "Rotary International",      abbr: "Rotary International" },
-  { name: "Rotaract District 3142",    abbr: "RID 3142"             },
-  { name: "Thane Municipal Corporation", abbr: "TMC"                },
-  { name: "Youth Action Network",      abbr: "YAN"                  },
-  { name: "Green Future Foundation",   abbr: "GFF"                  },
-  { name: "Skill India Mission",       abbr: "Skill India"          },
+  { name: "Rotaract District 3142",    abbr: "Rotaract District 3142"             },
+  { name: "Thane Municipal Corporation", abbr: "Thane Municipal Corporation"                },
 ]
